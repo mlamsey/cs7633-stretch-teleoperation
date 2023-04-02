f67eba9470b8646c3f88e9d080cec43de2b671b2
mlamsey <31256490+mlamsey@users.noreply.github.com>
2023-04-02 17:00:00 -0400

    Feature/drive bindings (#8)
    * checkin drive on xbox
    
    * checkin working drive bindings

--- nodes/xbox_teleop.py
+++ nodes/xbox_teleop.py
@@ -2,6 +2,7 @@
 import rospy
 
 from cs7633_project.xbox_control import XboxControl
+from cs7633_project.robot_control import ControllerAction
 from cs7633_project.srv import ControlAction, ControlActionRequest
 
 class XboxTeleop:
@@ -9,19 +10,36 @@ class XboxTeleop:
         rospy.init_node("xbox_teleop", anonymous=True)
         self.rate = rospy.Rate(10.)
         self.controller = XboxControl()
+        self.control_mode = ControlActionRequest.CONTROLLER_MANIPULATION
 
         # srv
         self.change_robot_pose_proxy = rospy.ServiceProxy(
             "/hri/control_action", ControlAction)
 
+    def swap_control_mode(self):
+        rospy.loginfo("Changing Control Mode!")
+        if self.control_mode == ControlActionRequest.CONTROLLER_MANIPULATION:
+            self.control_mode = ControlActionRequest.CONTROLLER_DRIVE
+            rospy.loginfo("Mode: DRIVE")
+        elif self.control_mode == ControlActionRequest.CONTROLLER_DRIVE:
+            self.control_mode = ControlActionRequest.CONTROLLER_MANIPULATION
+            rospy.loginfo("Mode: MANIPULATION")
+
     def main(self):
         while not rospy.is_shutdown():
-            action = self.controller.get_action(ControlActionRequest.CONTROLLER_MANIPULATION)
-            rospy.loginfo(action)
+            action = self.controller.get_action(self.control_mode)
+            if action == ControllerAction.CHANGE_MODE:
+                self.swap_control_mode()
+                rospy.loginfo("Changing Control Mode!")
+                self.rate.sleep()
+                continue
 
-            action = int(action.value)
-            state = int(ControlActionRequest.CONTROLLER_MANIPULATION)
-            self.change_robot_pose_proxy(action, state)
+            if action is not None:
+                # rospy.loginfo(action)
+
+                action = int(action.value)
+                state = int(self.control_mode)
+                self.change_robot_pose_proxy(action, state)
 
             self.rate.sleep()
 
